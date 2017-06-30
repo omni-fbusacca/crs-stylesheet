743071ebb03f93a3194b90e550a43dacea935d4a
enzomasciulli <enzo.masciulli@accenture.com>
2017-06-30 14:18:48 +0200

    ko-49 update

--- js/custom.js
+++ js/custom.js
@@ -1445,17 +1445,20 @@ function crsAccordion(){
 }
 function crsSameHeight(){
      var windowSize = $(window).width();
+     if (windowSize >= 768) {
+          $('.crs-type-content').each(function(){
+               var thisCrsWrapper = $(this).outerHeight();
+               $(this).find(".crs-same-height").css("min-height",thisCrsWrapper+"px");
+          });
+     }
      if($(".crs-type-content").legth>0) {
-        $('.crs-type-content').each(function(){
-            var thisCrsWrapper = $(this).outerHeight();
-            $(this).find(".crs-same-height").css("min-height",thisCrsWrapper+"px");
-        });
          $(window).resize(function(){
+             if (windowSize >= 768) {
                 $('.crs-type-content').each(function(){
                     var thisCrsWrapper = $(this).outerHeight();
                     $(this).find(".crs-same-height").css("min-height",thisCrsWrapper+"px");
                 });
-            
+            }
          })
      }
 }
