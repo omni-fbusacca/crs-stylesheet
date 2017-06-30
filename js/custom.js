13952104a8ff9e0c7f42195967601210b10954c5
enzomasciulli <enzo.masciulli@accenture.com>
2017-06-30 15:24:31 +0200

    ko-49 progress mobile

--- js/custom.js
+++ js/custom.js
@@ -1445,22 +1445,22 @@ function crsAccordion(){
 }
 function crsSameHeight(){
      var windowSize = $(window).width();
-     if (windowSize >= 768) {
-          $('.crs-type-content').each(function(){
-               var thisCrsWrapper = $(this).outerHeight();
-               $(this).find(".crs-same-height").css("min-height",thisCrsWrapper+"px");
-          });
-     }
+     
+    $('.crs-type-content').each(function(){
+        var thisCrsWrapper = $(this).outerHeight();
+        $(this).find(".crs-same-height").css("min-height",thisCrsWrapper+"px");
+    });
+     
      if($(".crs-type-content").length>0) {
          $(window).resize(function(){
              var windowSize = $(window).width();
-             if (windowSize >= 768) {
+             
                 $('.crs-type-content').each(function(){
                     $(this).find(".crs-same-height").css("min-height","auto");
                     var thisCrsWrapper = $(this).outerHeight();
                     $(this).find(".crs-same-height").css("min-height",thisCrsWrapper+"px");
                 });
-            }
+            
          })
      }
 }
