af0e172ec53c20b359f0f7dcf1f5a23dabe19883
ferdinando.busacca <ferdinando.busacca@accenture.com>
2017-07-12 15:44:45 +0200

    Validation messages fixed.

--- js/custom.js
+++ js/custom.js
@@ -2104,16 +2104,16 @@ Parsley.addMessages('it', {
 		alphanum:     "Questo valore deve essere di tipo alfanumerico."
 	},
 	notblank:       "Questo valore non deve essere vuoto.",
-	required:       "Questo valore Ã¨ richiesto.",
-	pattern:        "Questo valore non Ã¨ corretto.",
+	required:       "Questo valore è richiesto.",
+	pattern:        "Questo valore non è corretto.",
 	min:            "Questo valore deve essere maggiore di %s.",
 	max:            "Questo valore deve essere minore di %s.",
 	range:          "Questo valore deve essere compreso tra %s e %s.",
-	minlength:      "Questo valore Ã¨ troppo corto. La lunghezza minima Ã¨ di %s caratteri.",
-	maxlength:      "Questo valore Ã¨ troppo lungo. La lunghezza massima Ã¨ di %s caratteri.",
+	minlength:      "Questo valore è troppo corto. La lunghezza minima è di %s caratteri.",
+	maxlength:      "Questo valore è troppo lungo. La lunghezza massima è di %s caratteri.",
 	length:         "La lunghezza di questo valore deve essere compresa fra %s e %s caratteri.",
 	mincheck:       "Devi scegliere almeno %s opzioni.",
-	maxcheck:       "Devi scegliere al piÃ¹ %s opzioni.",
+	maxcheck:       "Devi scegliere al più %s opzioni.",
 	check:          "Devi scegliere tra %s e %s opzioni.",
 	equalto:        "Questo valore deve essere identico."
 });
